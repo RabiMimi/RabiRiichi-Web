6301e4f180dbb2c64a42fc1c072dc171c40040d4
KCFindstr <792491320@qq.com>
2026-06-28 22:46:43 -0700

    Remove frenqy server

--- src/config/servers.ts
+++ src/config/servers.ts
@@ -11,11 +11,6 @@ export const DEFAULT_SERVERS: DefaultServer[] = [
     url: 'wss://riichi-server.rabimimi.com',
   },
   {
-    id: 'frenqy',
-    nameKey: 'connect.frenqyServer',
-    url: 'wss://riichi-frenqy.rabimimi.com:5000',
-  },
-  {
     id: 'local',
     nameKey: 'connect.localServer',
     url: 'ws://localhost:5150',
